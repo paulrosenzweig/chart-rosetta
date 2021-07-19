e78785d0fcde4f2638c63085b7b629a8add97a0c
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2021-07-19 17:20:56 -0400

    hardcode things to make charts match better

--- charts/scatter-plots.js
+++ charts/scatter-plots.js
@@ -11,22 +11,22 @@ export async function plotD3(element) {
     }))
     .filter((c) => c.x != null && c.y != null);
   const { width, height } = element.getBoundingClientRect();
-  const margin = 30;
+  const margin = 70;
 
   const x = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.x)])
     .rangeRound([margin, width - margin])
-    .nice();
+    .nice(5);
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.y)])
     .rangeRound([height - margin, margin])
-    .nice();
+    .nice(5);
 
   const svg = d3.create("svg").attr("viewBox", `0 0 ${width} ${height}`);
 
-  for (const tick of x.ticks()) {
+  for (const tick of x.ticks(5)) {
     const [y1, y2] = y.domain().map(y);
     svg
       .append("line")
@@ -37,7 +37,7 @@ export async function plotD3(element) {
       .attr("stroke", "lightgray");
   }
 
-  for (const tick of y.ticks()) {
+  for (const tick of y.ticks(5)) {
     const [x1, x2] = x.domain().map(x);
     svg
       .append("line")
@@ -54,18 +54,43 @@ export async function plotD3(element) {
       .attr("cx", x(d.x))
       .attr("cy", y(d.y))
       .attr("r", 3)
-      .attr("fill", "steelblue");
+      .attr("fill", "steelblue")
+      .attr("opacity", 0.8);
   }
 
   svg
     .append("g")
     .attr("transform", `translate(${margin}, 0)`)
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y).ticks(5));
 
   svg
     .append("g")
     .attr("transform", `translate(0, ${height - margin})`)
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x).ticks(5));
+
+  const labels = svg
+    .append("g")
+    .style("font-family", "sans-serif")
+    .style("font-size", "12px");
+  const middleOfDomain = (x.domain()[0] + x.domain()[1]) / 2;
+  labels
+    .append("text")
+    .attr(
+      "transform",
+      `translate(${x(middleOfDomain)}, ${height - margin / 2})`
+    )
+    .attr("text-anchor", "middle")
+    .text("Horsepower");
+
+  const middleOfRange = (y.domain()[0] + y.domain()[1]) / 2;
+  labels
+    .append("text")
+    .attr(
+      "transform",
+      `translate(${margin / 2}, ${y(middleOfRange)}) rotate(-90)`
+    )
+    .attr("text-anchor", "middle")
+    .text("Displacement");
 
   element.append(svg.node());
 }
@@ -76,7 +101,7 @@ export async function plotECharts(element) {
 
   const option = {
     xAxis: { name: "Horsepower", nameLocation: "center" },
-    yAxis: { name: "Displacement", nameLocation: "center" },
+    yAxis: { name: "Displacement", nameLocation: "center", nameGap: 30 },
     series: [{ symbolSize: 5, data, type: "scatter" }],
   };
 
@@ -90,11 +115,21 @@ export async function plotVega(element) {
       "A scatterplot showing horsepower and miles per gallons for various cars.",
     width: "container",
     height: "container",
+    padding: 40,
     data: { values: carsDataset },
     mark: { type: "point", filled: true },
     encoding: {
-      x: { field: "Horsepower", type: "quantitative" },
-      y: { field: "Displacement", type: "quantitative" },
+      x: {
+        field: "Horsepower",
+        type: "quantitative",
+        scale: { nice: 6 },
+        axis: { tickCount: 6, titleFontWeight: 100, titleFontSize: 12 },
+      },
+      y: {
+        field: "Displacement",
+        type: "quantitative",
+        axis: { titleFontWeight: 100, titleFontSize: 12 },
+      },
     },
   };
   await vegaEmbed(element, spec);
